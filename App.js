ab66101eb833b6d8f1372c620a4b9f4d9dc40cf3
Jovan253 <54801590+Jovan253@users.noreply.github.com>
2023-02-21 22:38:40 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import {BrowserRouter} from 'react-router-dom';
+import { AuthWrapper } from './auth/AuthWrapper';
+
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <AuthWrapper />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
